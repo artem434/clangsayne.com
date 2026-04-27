fe9f7eef449176736ea17e727092fb3f388772c0
artem <artem43422322@gmail.com>
2026-04-27 10:12:51 +0100

    Checkpoint: demo mode for press and contact only

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,30 +1,9 @@
 module.exports = {
   nav: [
-    { label: "About", href: "/about.html" },
-    { label: "News", href: "/news.html" },
-    { label: "Works", href: "/works.html" },
-    { label: "Dates", href: "/dates.html" },
-    { label: "Shop", href: "/shop.html" },
     { label: "Press", href: "/press.html" },
     { label: "Contact", href: "/contact.html" },
-    { label: "Home", href: "/index.html" },
   ],
   socials: [
-    {
-      href: "http://clangsayne.bandcamp.com/releases",
-      icon: "sm-bc.png",
-      alt: "Bandcamp",
-      external: true,
-    },
-    { href: "https://twitter.com/clangsayne", icon: "sm-tw.png", alt: "Twitter", external: true },
-    { href: "https://www.facebook.com/clangsayne", icon: "sm-fb.png", alt: "Facebook", external: true },
-    {
-      href: "https://www.youtube.com/channel/UCTHujsPdgBEYNy-F0aUzfFg",
-      icon: "sm-yt.png",
-      alt: "YouTube",
-      external: true,
-    },
-    { href: "https://soundcloud.com/clangsayne", icon: "sm-sc.png", alt: "SoundCloud", external: true },
     { href: "/contact.html", icon: "sm-em.png", alt: "Contact", external: false },
   ],
 };
